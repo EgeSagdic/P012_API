615d024ea165ea4211ff83a816be1b6f915d4f92
egesa <egesa@LAPTOP-FEBL0K35>
2023-07-25 23:56:00 +0300

    Add project files.

---
+++ P012_API/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P012_API.Models
+{
+    public class DataContext:DbContext 
+    { 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-FEBL0K35\SQLEXPRESS;Database=P012_API;Trusted_Connection=true;encrypt=false");
+        }
+
+
+        public DbSet<Film> Film { get; set; }
+    }
+}
